1b79e4ee187203f37de0713857e7665f6f8ad105
devMiracle <rjcvjchzljv9@gmail.com>
2019-06-03 00:13:20 +0300

    concat name and email, save to file, read info from file.

--- StreamInputOutput/Program.cs
+++ StreamInputOutput/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;//input; output 
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,23 @@ namespace StreamInputOutput
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("test");
+            Console.WriteLine("Name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Email: ");
+            string email = Console.ReadLine();
+            string data = name + ":" + email;
+            FileInfo fileinfo = new FileInfo("clients.txt");
+            using (StreamWriter streamWriter = fileinfo.AppendText())
+            {
+                streamWriter.WriteLine(data);
+                streamWriter.Flush();
+            }
+
+            using (StreamReader streamReader = new StreamReader(fileinfo.FullName))
+            {
+                Console.WriteLine(streamReader.ReadToEnd());
+            }
+            Console.ReadKey();
         }
     }
 }
