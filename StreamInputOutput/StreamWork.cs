5194f8b7c6389eb7f4feb58867b7afbcc1e2c125
devMiracle <rjcvjchzljv9@gmail.com>
2019-06-08 21:34:08 +0300

    added method WriteFile

--- StreamInputOutput/StreamWork.cs
+++ StreamInputOutput/StreamWork.cs
@@ -3,11 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace StreamInputOutput
 {
     class StreamWork
     {
+        public static void WriteFile(string filePath, string text)
+        {
+            using (FileStream fs = new FileStream(filePath, FileMode.Create, FileAccess.Write, FileShare.None))
+            {
+                byte[] textByte = Encoding.Default.GetBytes(text);
+                fs.Write(textByte, 0, textByte.Length);
 
+            }
+
+        }
     }
 }
