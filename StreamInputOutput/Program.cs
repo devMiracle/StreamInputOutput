8bba5cb4453ed04c7faf9adf14e0452966bedfbf
devMiracle <rjcvjchzljv9@gmail.com>
2019-06-08 23:39:33 +0300

    Ready homework!

--- StreamInputOutput/Program.cs
+++ StreamInputOutput/Program.cs
@@ -9,19 +9,72 @@ namespace StreamInputOutput
 {
     class Program
     {
+        static void ClientDialog()
+        {
+            string text;
+            char input;
+            bool key;
+            do
+            {
+                key = false;
+                text = Console.ReadLine();
+                if (char.TryParse(text, out input))
+                    input = char.Parse(text);
+                else
+                {
+                    Console.WriteLine("Need one symbol. Try again(Y/n):");
+                    key = true;
+                    continue;
+                }
+
+                if (input != 'Y' && input != 'y' && input != 'N' && input != 'n')
+                {
+                    key = true;
+                    Console.WriteLine("unknown symbol. Try again(Y/n):");
+                }
+            } while (key);
+            if (input == 'N' || input == 'n')
+            {
+                Environment.Exit(0);
+            }
+            else if (input == 'Y' || input == 'y')
+            {
+                Console.WriteLine("Name: ");
+                string name = Console.ReadLine();
+                Console.WriteLine("Email: ");
+                string email = Console.ReadLine();
+                string data = name + " : " + email;
+                StreamWork.WriteFile(filePath, data);
+                Console.WriteLine("Data recorded!");
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine(StreamWork.ReadFile(filePath));
+                Console.ResetColor();
+            }
+        }
+        static string filePath = "data.txt";
         static void Main(string[] args)
         {
-            string filePath = "data.txt";
-            Console.WriteLine("Name: ");
-            string name = Console.ReadLine();
-            Console.WriteLine("Email: ");
-            string email = Console.ReadLine();
-            string data = name + ":" + email;
+            Console.SetWindowSize(60, 20);
+            Console.SetBufferSize(60, 20);
+            try
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine(StreamWork.ReadFile(filePath));
+                Console.ResetColor();
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("File not found!\nCreate new File?(Y/n):");
+                ClientDialog();
+            }
+            do
+            {
+                Console.WriteLine("Exchange info?(Y/n)");
+                ClientDialog();
 
-            StreamWork.WriteFile(filePath, data);
-            StreamWork.ReadFile(filePath);
-           
-            Console.ReadKey();
+            } while (true);
+
+
         }
     }
 }
--- StreamInputOutput/StreamWork.cs
+++ StreamInputOutput/StreamWork.cs
@@ -17,13 +17,14 @@ namespace StreamInputOutput
                     sw.WriteLine(text);
             }
         }
-        public static void ReadFile(string filePath)
+        public static string ReadFile(string filePath)
         {
             using (FileStream fs = new FileStream(filePath, FileMode.Open))
             {
                 using (StreamReader sr = new StreamReader(fs, Encoding.Unicode))
-                    Console.WriteLine(sr.ReadToEnd());
+                    return sr.ReadToEnd();
             }
         }
+        
     }
 }
